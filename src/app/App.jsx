d9a8dadc3920e172e5adb2d8255dc8c3e286522d
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-07-07 09:36:39 +0200

    first commit
    React website

---
+++ src/app/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Header from '../components/Header/Header.jsx';
+import Nav from '../components/Nav/Nav.jsx';
+import Hero from '../components/Hero/Hero.jsx';
+import AboutUs from '../components/AboutUs/AboutUs.jsx';
+import Main from '../components/Main/Main.jsx';
+import Footer from '../components/Footer/Footer.jsx';
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Hero />
+    <Nav />
+    <Main />
+    <AboutUs />
+    <Footer />
+    </>
+  )
+}
+
+export default App
